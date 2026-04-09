e8e55fda7d882409c05fc7517b9cce202ca9dfaa
Porchcar <78325054@qq.com>
2026-04-09 23:13:50 +0800

    修复权限检测异常

--- JiyuKiller.py
+++ JiyuKiller.py
@@ -24,7 +24,7 @@ Beta	#6c5ce7	测试紫
 RELEASE_TYPE = "Standard"
 # RELEASE_COLOR = "#2c68b6"
 RELEASE_COLOR = "#2c6fc7"
-VERSION = "3.3"
+VERSION = "3.3.1"
 
 import sys, os
 from pathlib import Path
@@ -281,8 +281,9 @@ class JiyuApp:
         if not askyesno("确认用户协议","这个程序用于理解极域电子教室、传奇电子教室等安全机制。\n该工具展示了可能被恶意利用的漏洞。请负责任地使用，且仅在受控环境中使用。\n\n该工具仅供教育和研究目的使用。\n我们不对该工具被用于恶意活动承担任何责任。\n我们与任何使用该代码进行商业活动的公司没有任何关联。\n\n同意用户协议并进入程序，点击“是”按钮。",icon="info",default="no"):
             sys.exit(0)
         
-        level = current_level()
-        showinfo("提示", f"启用 UIAccess超级置顶 功能失败，这可能由于多种原因。\n如果下列检测项均为正常，请与作者反馈并上传Log。\n\n打包为exe：{'是' if PACKED else '否（异常）'}\n权限：{level if level==2 else str(level)+'（异常）'}", icon="warning")
+        if not is_uiaccess():
+            level = current_level()
+            showinfo("提示", f"启用 UIAccess超级置顶 功能失败，这可能由于多种原因。\n如果下列检测项均为正常，请与作者反馈并上传Log。\n\n打包为exe：{'是' if PACKED else '否（异常）'}\n权限：{level if level==2 else str(level)+'（异常）'}", icon="warning")
         if args["--no-logging-file"]:
             print("Running main program...")
             self.build_ui()
@@ -900,6 +901,11 @@ os.chdir(WORK_DIR)  # 强制工作目录，解决_tkinter/PIL找不到
 
 # -------- 1. 判断当前令牌等级（你原版完全不变） --------
 def current_level():
+    try:
+        if is_uiaccess():  # 优先判断 UIAccess
+            return 3
+    except:
+        pass
     if "system" in subprocess.check_output("whoami", shell=True).decode().strip().lower():
         return 2
     if is_elevated():
